93b2c678f29ec3c058ce584eba57ade0834251b8
Freedom-Celebrate <freedomcelebrate01@gmail.com>
2026-04-09 01:13:29 +0100

    Add files via upload

---
+++ t2.go
@@ -0,0 +1,274 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func hex(s string) string {
+	var w []string = strings.Fields(s)
+
+	for i := 0; i < len(w); i++ {
+		if w[i] == "(hex)" && i > 0 {
+			pword := w[i-1]
+			hexstr, _ := strconv.ParseInt(pword, 16, 64)
+
+			w[i-1] = fmt.Sprint(hexstr)
+
+			w = append(w[:i], w[i+1:]...)
+			i--
+
+		}
+
+	}
+	return strings.Join(w, " ")
+}
+func bin(s string) string {
+	var w []string = strings.Fields(s)
+
+	for i := 0; i < len(w); i++ {
+		if w[i] == "(bin)" && i > 0 {
+			pword := w[i-1]
+			hexstr, _ := strconv.ParseInt(pword, 2, 64)
+
+			w[i-1] = fmt.Sprint(hexstr)
+
+			w = append(w[:i], w[i+1:]...)
+			i--
+
+		}
+
+	}
+	return strings.Join(w, " ")
+}
+
+func up(s string) string {
+	var w []string = strings.Fields(s)
+
+	for i := 0; i < len(w); i++ {
+		if w[i] == "(up)" && i > 0 {
+
+			w[i-1] = strings.ToUpper(w[i-1])
+
+			w = append(w[:i], w[i+1:]...)
+			i--
+
+		}
+
+	}
+	return strings.Join(w, " ")
+}
+
+func mulUp(s string) string {
+	w := strings.Fields(s)
+
+	for i := 0; i < len(w); i++ {
+		if strings.HasPrefix(w[i], "(up,") {
+			var number string
+			if strings.HasSuffix(w[i], ")") {
+				number = strings.TrimSuffix(strings.TrimPrefix(w[i], "(up,"), ")")
+				w = append(w[:i], w[i+1:]...)
+				i--
+
+				count, _ := strconv.Atoi(number)
+
+				start := i - count + 1
+				if start < 0 {
+					start = 0
+				}
+				for j := start; j <= i; j++ {
+					w[j] = strings.ToUpper(w[j])
+				}
+			}
+		}
+
+	}
+	return strings.Join(w, " ")
+
+}
+
+func mulLow(s string) string {
+	w := strings.Fields(s)
+
+	for i := 0; i < len(w); i++ {
+		if strings.HasPrefix(w[i], "(low,") {
+			var number string
+			if strings.HasSuffix(w[i], ")") {
+				number = strings.TrimSuffix(strings.TrimPrefix(w[i], "(low,"), ")")
+				w = append(w[:i], w[i+1:]...)
+				i--
+
+				count, _ := strconv.Atoi(number)
+
+				start := i - count + 1
+				if start < 0 {
+					start = 0
+				}
+				for j := start; j <= i; j++ {
+					w[j] = strings.ToLower(w[j])
+				}
+			}
+		}
+
+	}
+	return strings.Join(w, " ")
+
+}
+func mulCap(s string) string {
+	w := strings.Fields(s)
+
+	for i := 0; i < len(w); i++ {
+		if strings.HasPrefix(w[i], "(cap,") {
+			var number string
+			if strings.HasSuffix(w[i], ")") {
+				number = strings.TrimSuffix(strings.TrimPrefix(w[i], "(cap,"), ")")
+				w = append(w[:i], w[i+1:]...)
+				i--
+
+				count, _ := strconv.Atoi(number)
+
+				start := i - count + 1
+				if start < 0 {
+					start = 0
+				}
+				for j := start; j <= i; j++ {
+					w[j] = strings.ToUpper(string(w[j][0])) + strings.ToLower(string(w[j][1:]))
+
+				}
+			}
+		}
+
+	}
+	return strings.Join(w, " ")
+
+}
+
+// func mulUp(s string) string {
+// 	w := strings.Fields(s)
+
+// 	for i := 0; i < len(w); i++ {
+// 		if strings.HasPrefix(w[i], "(up,") && strings.HasSuffix(w[i], ")") {
+// 			w[i] = strings.TrimPrefix(w[i], "(up,")
+// 			w[i] = strings.TrimSuffix(w[i], ")")
+// 			w = append(w[:i], w[i+1:]...)
+// 			i--
+
+// 			count, _ := strconv.Atoi(w[i])
+
+// 			start := i - count + 1
+// 			if start < 0 {
+// 				start = 0
+// 			}
+// 			for j := start; j <= i; j++ {
+// 				w[j] = strings.ToUpper(w[j])
+// 			}
+// 		}
+
+// 	}
+// 	return strings.Join(w, " ")
+
+// }
+func GoisFun(a string) []string {
+	s := strings.Fields(a)
+	var result []string
+	for i := 0; i < len(s); i++ {
+		if s[i] == "(up)" && i > 0 {
+			result[i-1] = strings.ToUpper(result[i-1])
+			continue
+		}
+		result = append(result, s[i])
+	}
+	return result
+}
+
+func low(s string) string {
+	var w []string = strings.Fields(s)
+
+	for i := 0; i < len(w); i++ {
+		if w[i] == "(low)" && i > 0 {
+
+			w[i-1] = strings.ToUpper(w[i-1])
+
+			w = append(w[:i], w[i+1:]...)
+			i--
+
+		}
+
+	}
+	return strings.Join(w, " ")
+}
+
+func cap(s string) string {
+	var w []string = strings.Fields(s)
+
+	for i := 0; i < len(w); i++ {
+		if w[i] == "(cap)" && i > 0 {
+
+			w[i-1] = strings.ToUpper(string(w[i-1][0])) + strings.ToLower(string(w[i-1][1:]))
+
+			w = append(w[:i], w[i+1:]...)
+			i--
+
+		}
+
+	}
+	return strings.Join(w, " ")
+}
+
+// func fixarticles(s string) string {
+// 	var w []string = strings.Fields(s)
+
+// 	for i := 0; i < len(w); i++ {
+
+// 		if i < len(w) {
+// 			if w[i] == "a" && strings.ContainsAny("aeiouh", string(string(w[i+1][0]))) {
+
+// 				w[i] = "an"
+
+// 			} else if w[i] == "A" && strings.ContainsAny("AEIOUH", string(string(w[i+1][0]))) {
+// 				w[i] = "An"
+// 			} else if w[i] == "an" && !strings.ContainsAny("AEIOUH", string(string(w[i+1][0]))) {
+// 				w[i] = "a"
+// 			}
+// 		}
+// 	}
+// 	return strings.Join(w, " ")
+// }
+
+func fixarticles(s string) string {
+	words := strings.Fields(s)
+	vowels := "aeiouhAEIOUH"
+
+	for i := 0; i < len(words); i++ {
+		if i < len(words) {
+			word := words[i]
+			if word == "a" && strings.ContainsAny(vowels, string(string(words[i+1][0]))) {
+				words[i] = "an"
+			} else if word == "A" && strings.ContainsAny(vowels, string(string(words[i+1][0]))) {
+				words[i] = "An"
+			} else if word == "an" && !strings.ContainsAny(vowels, string(string(words[i+1][0]))) {
+				words[i] = "a"
+			}
+		}
+
+	}
+
+	return strings.Join(words, " ")
+
+}
+func fixpunctuation(s string) string {
+
+}
+func main() {
+	fmt.Println(hex("1E (hex) files were added"))
+	fmt.Println(bin("10 (bin) files were added"))
+	fmt.Println(GoisFun("dgf (up) files were added"))
+	fmt.Println(low("sdf SDK (low) files were added"))
+	fmt.Println(cap("sdf SDK (cap) files were added"))
+	fmt.Println(mulUp("sdf dsg dsgfd files (up,2) were added"))
+	fmt.Println(mulLow("ASDF SDF SFGD (low,3) were added"))
+	fmt.Println(mulCap("ASDF SDF SFGD (cap,3) were added"))
+	fmt.Println(fixarticles("A apple an book"))
+
+}
